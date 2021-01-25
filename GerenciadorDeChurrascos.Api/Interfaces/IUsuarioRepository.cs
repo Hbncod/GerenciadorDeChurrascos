ae89ce04dabad4a682d097feb3de45a2fe700331
Hbncod <hbncod@gmail.com>
2021-01-25 00:21:07 -0300

    Crud Usuario

--- GerenciadorDeChurrascos.Api/Interfaces/IUsuarioRepository.cs
+++ GerenciadorDeChurrascos.Api/Interfaces/IUsuarioRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GerenciadorDeChurrascos.Api.Domains;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,40 @@ namespace GerenciadorDeChurrascos.Api.Interfaces
 {
     interface IUsuarioRepository
     {
+        /// <summary>
+        /// Lista os usuários
+        /// </summary>
+        /// <returns>Uma lista de usuario</returns>
+        List<UsuarioDomain> Listar();
+        /// <summary>
+        /// Lista um usuário pelo seu id
+        /// </summary>
+        /// <param name="id">Id do usuario a ser buscado</param>
+        /// <returns>O usuário do id informado</returns>
+        UsuarioDomain BuscarPorId(string id);
+        /// <summary>
+        /// Cadastra um novo usuário
+        /// </summary>
+        /// <param name="novoUsuario">Dados do usuário a ser cadastrado</param>
+        void Cadastrar(UsuarioDomain novoUsuario);
+        /// <summary>
+        /// Atualiza um usuário
+        /// </summary>
+        /// <param name="id">Id do usuário a ser atualizado</param>
+        /// <param name="usuarioAtualizado">dados do usuário atualizado</param>
+        void Atualizar(string id, UsuarioDomain usuarioAtualizado);
+        /// <summary>
+        /// Deleta um usuário pel seu id
+        /// </summary>
+        /// <param name="id">Id do usuário a ser deletado</param>
+        void Deletar(string id);
+        /// <summary>
+        /// Faz o login do usuário
+        /// </summary>
+        /// <param name="email">email para ser comparado ao realizar o login</param>
+        /// <param name="senha">senha a ser comparada ao realizar o login</param>
+        /// <returns></returns>
+        UsuarioDomain Login(string email, string senha);
+
     }
 }
--- GerenciadorDeChurrascos.Api/Repositories/UsuarioRepository.cs
+++ GerenciadorDeChurrascos.Api/Repositories/UsuarioRepository.cs
@@ -1,12 +1,50 @@
-﻿using GerenciadorDeChurrascos.Api.Interfaces;
-using System;
+﻿using GerenciadorDeChurrascos.Api.Contexts;
+using GerenciadorDeChurrascos.Api.Domains;
+using GerenciadorDeChurrascos.Api.Interfaces;
+using MongoDB.Driver;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace GerenciadorDeChurrascos.Api.Repositories
 {
     public class UsuarioRepository : IUsuarioRepository
     {
+        private readonly IMongoCollection<UsuarioDomain> _usuario;
+        public UsuarioRepository(IChurrascostoreDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _usuario = database.GetCollection<UsuarioDomain>(settings.UsuariosCollectionName);
+        }
+
+        public void Atualizar(string id, UsuarioDomain usuarioAtualizado)
+        {
+            _usuario.ReplaceOne(u => u.Id == id, usuarioAtualizado);
+        }
+
+        public UsuarioDomain BuscarPorId(string id)
+        {
+            return _usuario.Find(u => u.Id == id).First();
+        }
+
+        public void Cadastrar(UsuarioDomain novoUsuario)
+        {
+            _usuario.InsertOne(novoUsuario);
+        }
+
+        public void Deletar(string id)
+        {
+            _usuario.DeleteOne(u => u.Id == id);
+        }
+
+        public List<UsuarioDomain> Listar()
+        {
+            return _usuario.AsQueryable<UsuarioDomain>().ToList();
+        }
+
+        public UsuarioDomain Login(string email, string senha)
+        {
+            return _usuario.Find(u => u.Email == email && u.Senha == senha).First();
+        }
     }
 }
